cc082eecf3237c1a05282685ecb0d65497d1c8df
Omnia Gamal <96677695+omniagamal5588@users.noreply.github.com>
2023-05-29 22:24:20 +0300

    Update urls.py

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,15 @@
 from django.urls import path,include
-from account.views import UserRegistrationView,UserLoginView,UserProfileView,UserPasswordResetView,RestPasswordView,SendPasswordResetEmailView
+from account.views import UserRegistrationView,UserLoginView,LogOutView,UserProfileView,RestPasswordView,ForgetPasswordView
 
 urlpatterns=[
   path('register/',UserRegistrationView.as_view(),name='register'),
   path('login/',UserLoginView.as_view(),name='login'),
   path('profile/', UserProfileView.as_view(), name='profile'),
   path('resetPassword/', RestPasswordView.as_view(), name='resetPassword'),
-  path('send-reset-password-email/', SendPasswordResetEmailView.as_view(), name='send-reset-password-email'),
-  path('reset-password/<uid>/<token>/', UserPasswordResetView.as_view(), name='reset-password'),  
+  path('forgetPassword/',ForgetPasswordView.as_view(),name='forgetPassword'),
+  path('editProfile/', UserProfileView.as_view(), name='profile'),
+   path('logout/', LogOutView.as_view(), name='logout'),
+  
+  # path('send-reset-password-email/', SendPasswordResetEmailView.as_view(), name='send-reset-password-email'),
+  # path('reset-password/<uid>/<token>/', UserPasswordResetView.as_view(), name='reset-password'),  
 ]
