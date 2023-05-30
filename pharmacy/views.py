e7ee3d53b327585183db7338f8668879ec533729
Omnia Gamal <96677695+omniagamal5588@users.noreply.github.com>
2023-05-30 12:23:44 +0300

    Update views.py

--- pharmacy/views.py
+++ pharmacy/views.py
@@ -39,39 +39,39 @@ def getSubscriotionToken(user, sub_type, startDate, endDate):
 def isLogin(request):
   token = request.META.get('HTTP_AUTHORIZATION')
   if not token:
-    raise AuthenticationFailed('Authentication credentials were not provided.')
+    raise AuthenticationFailed({"success":False,'message':'Authentication credentials were not provided.'})
   try:
     payload = jwt.decode(token, 'secret', algorithms=['HS256'])
   except jwt.ExpiredSignatureError:
-    raise AuthenticationFailed('Authentication credentials were not provided.')
+    raise AuthenticationFailed({"success":False,'message':'Authentication credentials were not provided.'})
   
   except jwt.exceptions.DecodeError:
-    raise AuthenticationFailed('Invalid token')
+    raise AuthenticationFailed({"success":False,"message":'Invalid token'})
 
 
   user = Pharmacy.objects.filter(id=payload['id']).first()
   if not user:
-    raise AuthenticationFailed('User Account not found!')
+    raise AuthenticationFailed({"success":False,'message':'User Account not found!'})
   
   return user
 
 def isSubscribe(request):
   token =request.META.get('HTTP_AUTHORIZATION')
   if not token:
-    raise AuthenticationFailed('Authentication credentials were not provided.')
+    raise AuthenticationFailed({"success":False,'message':'Authentication credentials were not provided.'})
   try:
     payload = jwt.decode(token, 'secret', algorithms=['HS256'])
   except jwt.ExpiredSignatureError:
-    raise AuthenticationFailed('Subscription is Expired')
+    raise AuthenticationFailed({"success":False,'message':'Subscription is Expired'})
   
   except jwt.exceptions.DecodeError:
-    raise AuthenticationFailed('Invalid token')
+    raise AuthenticationFailed({"success":False,'message':'Invalid token'})
   
   user = Pharmacy.objects.filter(id=payload['id']).first()
   if not user:
-    raise AuthenticationFailed('User Account not found!')
+    raise AuthenticationFailed({"success":False,'message':'User Account not found!'})
   elif payload['role'] !="subscribe":
-    raise AuthenticationFailed("You aren't Authorized, you must make Subscription")
+    raise AuthenticationFailed({"success":False,'message':"You aren't Authorized, you must make Subscription"})
   
   return user
   
@@ -82,22 +82,22 @@ class PharmacyRegistrationView(APIView):
     serializer = PharmacyRegistrationSerializer(data=request.data)
     if serializer.is_valid(raise_exception=True):
         pharmacy = serializer.save()
-        return Response({'msg':'Registration Of Pharmacy Successfully',"success":True}, status=status.HTTP_201_CREATED)
+        return Response({'message':'Registration Of Pharmacy Successfully',"success":True}, status=status.HTTP_201_CREATED)
     print(serializer.errors)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
-
+    # raise AuthenticationFailed ({"success":False,'massage':'This Email already Exist'})
 
 #login Pharmacy View
 class PharmacyLoginView(APIView):
   def post(self, request):
     if 'email' not in request.data and 'password' not in request.data:
-      return Response({"errors": {"email": ["this field is required"], "password": ["this field is required"]}}, status=status.HTTP_400_BAD_REQUEST)
+      return Response({"success":False,"message": {"email": ["this field is required"], "password": ["this field is required"]}}, status=status.HTTP_400_BAD_REQUEST)
     
     elif 'email' not in request.data:
-      return Response({"errors": {"email": ["this field is required"]}}, status=status.HTTP_400_BAD_REQUEST)
+      return Response({"success":False,"message": {"email": ["this field is required"]}}, status=status.HTTP_400_BAD_REQUEST)
     
     elif 'password' not in request.data:
-      return Response({"errors": {"password": ["this field is required"]}}, status=status.HTTP_400_BAD_REQUEST)
+      return Response({"success":False,"message": {"password": ["this field is required"]}}, status=status.HTTP_400_BAD_REQUEST)
     
     email = request.data['email']
     password = request.data['password']
@@ -105,10 +105,10 @@ class PharmacyLoginView(APIView):
     pharmacy = Pharmacy.objects.filter(email=email).first()
 
     if pharmacy is None:
-        raise AuthenticationFailed('Pharmacy Account not found!')
+        raise AuthenticationFailed({'message':'Pharmacy Account not found!',"success": False})
 
     if not pharmacy.check_password(password):
-        raise AuthenticationFailed('Incorrect password!')
+        raise AuthenticationFailed({'success':False,'message':'Incorrect password!'})
     
     payload = {
           'id': pharmacy.id,
@@ -155,7 +155,7 @@ class PharmacyProfileView(APIView):
     serializer = PharmacyProfileSerializer(user, data=request.data)
     if serializer.is_valid(raise_exception=True):
         pharmacy = serializer.save()
-        return Response({'msg':'profile updated Successfully',"success":True}, status=status.HTTP_201_CREATED)
+        return Response({'message':'profile updated Successfully',"success":True}, status=status.HTTP_201_CREATED)
     print(serializer.errors)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST) 
 
@@ -165,16 +165,16 @@ class RestPasswordView(APIView):
   renderer_classes = [PharmacyRenderer]
   def post(self, request, format=None):
     if 'old_password' not in request.data and 'new_password' not in request.data:
-      return Response({"errors": {"old_password": ["this field is required"], "new_password": ["this field is required"]}}, status=status.HTTP_400_BAD_REQUEST)
+      return Response({"success":False,"message": {"old_password": ["this field is required"], "new_password": ["this field is required"]}}, status=status.HTTP_400_BAD_REQUEST)
     
     # elif 'email' not in request.data:
     #   return Response({"errors": {"email": ["this field is required"]}}, status=status.HTTP_400_BAD_REQUEST)
     
     elif 'old_password' not in request.data:
-      return Response({"errors": {"old_password": ["this field is required"]}}, status=status.HTTP_400_BAD_REQUEST)
+      return Response({"success":False,"message": {"old_password": ["this field is required"]}}, status=status.HTTP_400_BAD_REQUEST)
     
     elif 'new_password' not in request.data:
-      return Response({"errors": {"new_password": ["this field is required"]}}, status=status.HTTP_400_BAD_REQUEST)
+      return Response({"success":False,"message": {"new_password": ["this field is required"]}}, status=status.HTTP_400_BAD_REQUEST)
     
     old_password = request.data['old_password']
     user = isSubscribe(request)
@@ -182,7 +182,7 @@ class RestPasswordView(APIView):
     flag = user.check_password(old_password)
 
     if not flag:
-      return Response({"errors": {"old_password": ["this field is invalid"]}}, status=status.HTTP_400_BAD_REQUEST)
+      return Response({"success":False,"message": {"old_password": ["this field is invalid"]}}, status=status.HTTP_400_BAD_REQUEST)
     
     user.set_password(new_password)
     user.save()
@@ -193,16 +193,16 @@ class ForgetPasswordView(APIView):
    renderer_classes = [PharmacyRenderer]
    def put(self,request,formt=None):
        if 'email' not in request.data:
-        return Response({"errors": {"email": ["this field is required"]}}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"success":False,"message": {"email": ["this field is required"]}}, status=status.HTTP_400_BAD_REQUEST)
        
        elif 'new_password' not in request.data:
-        return Response({"errors": {"new_password": ["this field is required"]}}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"success":False,"message": {"new_password": ["this field is required"]}}, status=status.HTTP_400_BAD_REQUEST)
   
        email = request.data['email']
        new_password = request.data['new_password']
        user = Pharmacy.objects.filter(email=email).first()
        if not user:
-        raise AuthenticationFailed('Pharmacy Account not found!')
+        raise AuthenticationFailed({"success":False,'message':'Pharmacy Account not found!'})
        user.set_password(new_password)
        user.save()
        return Response({"success":True, "message": "Password is changed Successfully"}, status=status.HTTP_200_OK)
@@ -222,11 +222,11 @@ class submitSubscriptionView(APIView):
   def post(self,request,format=None):
     user = isLogin(request)
     if 'subscription_id' not in request.data:
-      return Response({'success':False,'subscription_id':'subscription_id is Requried'})
+      return Response({"success":False,'message':'subscription_id is Requried'})
     
     sub_type=Subscription.objects.filter(id=request.data["subscription_id"]).first()
     if not sub_type:
-       return Response ({'success':False,'msg':'Subscription Type not Avabliable'})
+       return Response ({"success":False,'message':'Subscription Type not Avabliable'})
     
     payload = {
         'pharmacy_id': user.id,
@@ -241,7 +241,7 @@ class submitSubscriptionView(APIView):
     end=datetime.datetime.utcnow()+datetime.timedelta(days=sub_type.duration) 
     start =datetime.datetime.utcnow()
     Subscription_Pharmacy.objects.create(pharmacy_id=user,subscription_id=sub_type,start_date=start,end_date=end)
-    return JsonResponse({"msg":"the subscription done",'subscriptionToken':token,'success':True})
+    return JsonResponse({"message":"the subscription done",'subscriptionToken':token,'success':True})
     # return JsonResponse({"error":"the user is not authorized to do it"})    
 
 #Crud Opertaion For Medicine APIView
@@ -268,9 +268,9 @@ class MedicineDetailes(APIView):
             medicine=serializer.save()
          exist= Pharmacy_medicine.objects.filter(pharmacy_id=user,medicine_id=medicine).first()
          if exist:
-            return Response({'success':False,"msg":"Medicine Is Already Exist"},status=status.HTTP_400_BAD_REQUEST)
+            return Response({'success':False,"message":"Medicine Is Already Exist"},status=status.HTTP_400_BAD_REQUEST)
          Pharmacy_medicine.objects.create(pharmacy_id=user,medicine_id=medicine)    
-         return Response({"msg":"Medicine Succesfully Added",'success':True},status=status.HTTP_201_CREATED)
+         return Response({"message":"Medicine Succesfully Added",'success':True},status=status.HTTP_201_CREATED)
       
       return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
@@ -283,7 +283,7 @@ class MedicineInfo(APIView):
       try:
          obj=Medicine.objects.get(id=id)
       except Medicine.DoesNotExist:
-         msg={"msg":"This Medicine Type Not Found", "success":False} 
+         msg={"message":"This Medicine Type Not Found", "success":False} 
          return Response(msg,status=status.HTTP_404_NOT_FOUND)
       serializer =MedicineSerializer(obj)
       return Response({"success":True, "data":serializer.data},status=status.HTTP_200_OK)
@@ -299,7 +299,7 @@ class MedicineInfo(APIView):
       if serializer.is_valid():
          medicine=Medicine.objects.filter(serial_number=request.data['serial_number']).first()
          if medicine:
-            return Response({'success':False,"msg":"Is Already Exist"},status=status.HTTP_400_BAD_REQUEST)
+            return Response({'success':False,"message":"Is Already Exist"},status=status.HTTP_400_BAD_REQUEST)
          serializer.save()
          return Response({"success":True ,"data":serializer.data},status=status.HTTP_205_RESET_CONTENT)
       return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
@@ -310,10 +310,10 @@ class MedicineInfo(APIView):
       try:
          obj=Medicine.objects.get(id=id)
       except Medicine.DoesNotExist:
-         msg={"msg":"Medicine Not Found", "success":False}
+         msg={"message":"Medicine Not Found", "success":False}
          return Response(msg,status=status.HTTP_404_NOT_FOUND)
       obj.delete()
-      return Response({"msg":"deleted successfully", "success":True},status=status.HTTP_204_NO_CONTENT)
+      return Response({"message":"deleted successfully", "success":True},status=status.HTTP_204_NO_CONTENT)
    
 #Offers Section
 
@@ -331,13 +331,13 @@ class OffersDetailes(APIView):
       user = isSubscribe(request)
       exist= Pharmacy_medicine.objects.filter(pharmacy_id=user,medicine_id=id).first()
       if not exist:
-        return Response({'success':False,'msg':"You Do'nt Have this Medicine"},status=status.HTTP_400_BAD_REQUEST)
+        return Response({'success':False,'message':"You Do'nt Have this Medicine"},status=status.HTTP_400_BAD_REQUEST)
       
       #entryData={"pharmacy"}
       serializer=PharmacyMedicineSerializer(exist,data=request.data)
       if serializer.is_valid():
          serializer.save()
-         return Response({'success':True,'msg':'offer is added'},status=status.HTTP_201_CREATED)
+         return Response({'success':True,'message':'offer is added'},status=status.HTTP_201_CREATED)
       return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
    
 #For Get&Put&Patch&Delete On object
@@ -347,8 +347,8 @@ class OfferInfo(APIView):
         try:
           obj=Offers.objects.get(id=id)
         except Offers.DoesNotExist:
-          msg={"msg":"This Offer Post Not Found"} 
-          return Response(msg,status=status.HTTP_404_NOT_FOUND)
+          message={"message":"This Offer Post Not Found"} 
+          return Response(message,status=status.HTTP_404_NOT_FOUND)
         serializer =OfferSerializer(obj)
         return Response(serializer.data,status=status.HTTP_200_OK)
 
@@ -357,8 +357,8 @@ class OfferInfo(APIView):
         try:
           obj=Offers.objects.get(id=id)
         except Offers.DoesNotExist:
-          msg={"msg":"Not Found "}
-          return Response(msg,status=status.HTTP_404_NOT_FOUND)
+          message={"message":"Not Found "}
+          return Response(message,status=status.HTTP_404_NOT_FOUND)
         serializer =OfferSerializer(obj,data=request.data)
         if serializer.is_valid():
           serializer.save()
@@ -370,10 +370,10 @@ class OfferInfo(APIView):
         try:
           obj=Offers.objects.get(id=id)
         except Offers.DoesNotExist:
-          msg={"msg":"Not Found"}
-          return Response(msg,status=status.HTTP_404_NOT_FOUND)
+          message={"message":"Not Found"}
+          return Response(message,status=status.HTTP_404_NOT_FOUND)
         obj.delete()
-        return Response({"msg":"deleted"},status=status.HTTP_204_NO_CONTENT)
+        return Response({"message":"deleted"},status=status.HTTP_204_NO_CONTENT)
 
 
 class SubscriptionPlanView(APIView):
