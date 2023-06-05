37eb0223799ea03bdc7f032a3398e095d4dfd5ba
Omnia Gamal <96677695+omniagamal5588@users.noreply.github.com>
2023-06-05 13:38:05 +0300

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -20,5 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('account.urls')),
-    path('api/pharmacy/', include('pharmacy.urls'))
+    path('api/pharmacy/', include('pharmacy.urls')),
+    path('img/', include('img.urls')),
+    
 ]
