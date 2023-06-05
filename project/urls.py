8b635b64462c8a2c1e4c472446190da7d78d1bba
Omnia Gamal <96677695+omniagamal5588@users.noreply.github.com>
2023-06-05 14:03:10 +0300

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -21,6 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/user/', include('account.urls')),
     path('api/pharmacy/', include('pharmacy.urls')),
-    path('img/', include('img.urls')),
+   
     
 ]
