191782b7d6225d95ae5105c38904e445fc3be78b
eman-m0hamed <91141798+eman-m0hamed@users.noreply.github.com>
2023-06-12 18:35:14 +0300

    Update admin.py

--- pharmacy/admin.py
+++ pharmacy/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from pharmacy.models import Pharmacy
+from pharmacy.models import *
 # Register your models here.
 
 admin.site.register(Pharmacy)
+admin.site.register(Subscription)
+admin.site.register(Subscription_Pharmacy)
+admin.site.register(Medicine)
+admin.site.register(Pharmacy_medicine)
