85d5ed563ec560a813b95f54c1e17ea2c90accb7
eman-m0hamed <91141798+eman-m0hamed@users.noreply.github.com>
2023-06-12 18:28:09 +0300

    Update urls.py

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path,include
-from account.views import UserRegistrationView,UserLoginView,LogOutView,UserProfileView,RestPasswordView,ForgetPasswordView
-
+from account.views import *
 urlpatterns=[
   path('register/',UserRegistrationView.as_view(),name='register'),
   path('login/',UserLoginView.as_view(),name='login'),
@@ -8,8 +7,10 @@ urlpatterns=[
   path('resetPassword/', RestPasswordView.as_view(), name='resetPassword'),
   path('forgetPassword/',ForgetPasswordView.as_view(),name='forgetPassword'),
   path('editProfile/', UserProfileView.as_view(), name='profile'),
-   path('logout/', LogOutView.as_view(), name='logout'),
-  
+  path('logout/', LogOutView.as_view(), name='logout'),
+  path('admin/', AdminView.as_view(), name='logout'),
+
+
   # path('send-reset-password-email/', SendPasswordResetEmailView.as_view(), name='send-reset-password-email'),
-  # path('reset-password/<uid>/<token>/', UserPasswordResetView.as_view(), name='reset-password'),  
+  # path('reset-password/<uid>/<token>/', UserPasswordResetView.as_view(), name='reset-password'),
 ]
