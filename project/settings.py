136c93d75bce9749c29a5f5672eb0896d2f4ee71
eman-m0hamed <91141798+eman-m0hamed@users.noreply.github.com>
2023-06-12 23:30:50 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -106,7 +106,7 @@ REST_FRAMEWORK = {
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
+DATABASES = {
 
      'default': {
      'ENGINE': 'django.db.backends.mysql',
