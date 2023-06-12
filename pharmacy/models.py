39b73666a58a9eca9b18078a97729a68b05e81c0
eman-m0hamed <91141798+eman-m0hamed@users.noreply.github.com>
2023-06-12 18:35:38 +0300

    Update models.py

--- pharmacy/models.py
+++ pharmacy/models.py
@@ -2,49 +2,19 @@ from django.db import models
 from django.contrib.auth.models import PermissionsMixin
 from django.contrib.auth.models import BaseUserManager,AbstractBaseUser
 from django.utils import timezone
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 #  Custom User Manager
 class UserManager(BaseUserManager):
-  def create_user(self, email, name, location, phone_number, description,pharmacy_image ,password=None):
-      """
-      Creates and saves a User with the given email, name, tc and password.
-      """
-      if not email:
-        raise ValueError('User must have an email address')
+   def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-      user = self.model(
-          email=self.normalize_email(email),
-         name=name,
-         location=location,
-         phone_number=phone_number,
-         description=description,
-         pharmacy_image = pharmacy_image
-         
-      )
-      user.set_password(password)
-      user.save(using=self._db)
-      return user
-
-  def create_superuser(self, email, name, location,phone_number,description, pharmacy_image, password=None):
-      """
-      Creates and saves a superuser with the given email, name, tc and password.
-      """
-      user = self.create_user(
-          email=self.normalize_email(email),
-          password=password,
-          name=name,
-          location=location,
-          phone_number=phone_number,
-          description=description,
-          pharmacy_image=pharmacy_image
-          
-
-      )
-      user.is_admin = True
-      user.save(using=self._db)
-      return user
-      
 #  Custom User Model
 class Pharmacy(AbstractBaseUser):
   email = models.EmailField(
@@ -56,7 +26,7 @@ class Pharmacy(AbstractBaseUser):
   location=models.CharField(max_length=200, verbose_name='Location')
   phone_number=models.CharField(max_length=15, verbose_name='Phone number')
   description=models.CharField(max_length=255,verbose_name='Description')
-  pharmacy_image=models.ImageField(upload_to='img', null=False ,verbose_name='Pharmacy Image')
+  pharmacy_image=models.ImageField(upload_to='pharmacy_pictures', null=True ,verbose_name='Pharmacy Image')
   is_active = models.BooleanField(default=True)
   is_superuser = models.BooleanField(default=False)
   created_at = models.DateTimeField(auto_now_add=True)
@@ -69,47 +39,42 @@ class Pharmacy(AbstractBaseUser):
 
   def __str__(self):
       return self.email
-  
 
-# Subscrition Type
-# class Subscription_Type(models.Model):
-#     name=models.CharField(max_length=100)
 
 #Subscription Class
 class Subscription(models.Model):
     price=models.IntegerField(null=False)
-    #user = models.ForeignKey(Pharmacy, on_delete=models.CASCADE)
     subscription_type = models.CharField( max_length=255,null=False)
     duration=models.IntegerField(null=False)
-    
-   
+
+
 
 
 class Subscription_Pharmacy(models.Model):
-    pharmacy_id=models.ForeignKey(Pharmacy,on_delete=models.SET_NULL,null=True )
-    subscription_id=models.ForeignKey(Subscription,on_delete=models.SET_NULL,null=True )
+    pharmacy_id=models.ForeignKey(Pharmacy,on_delete=models.CASCADE)
+    subscription_id=models.ForeignKey(Subscription,on_delete=models.CASCADE)
     start_date = models.DateTimeField(default=timezone.now)
-    end_date = models.DateTimeField(null=True)  
+    end_date = models.DateTimeField(null=True)
 
 
 class Medicine(models.Model):
-  serial_number=models.CharField(max_length=255 )
-  medicine_image=models.ImageField(upload_to='img', null=False ,verbose_name='Medicine Image')
+  serial_number=models.CharField(max_length=255)
   medicine_name=models.CharField(max_length=255)
   medicine_description=models.TextField()
   medicine_price=models.DecimalField(max_digits=6, decimal_places=2)
+  medicine_image=models.ImageField(upload_to='medicine_pictures', null=True ,verbose_name='Medicine Image')
   pharmacy_id=models.CharField(Pharmacy,max_length=255,default=True)
-  
+
 
 class Pharmacy_medicine(models.Model):
-  pharmacy_id=models.ForeignKey(Pharmacy,on_delete=models.SET_NULL,null=True)
-  medicine_id=models.ForeignKey(Medicine,on_delete=models.SET_NULL,null=True)
-  offer = models.IntegerField(default=0)
+  pharmacy_id=models.ForeignKey(Pharmacy,on_delete=models.CASCADE)
+  medicine_id=models.ForeignKey(Medicine,on_delete=models.CASCADE)
+  offer = models.IntegerField(default=0,validators=[MinValueValidator(0), MaxValueValidator(100)])
 
-  
+
 
 #   def __str__(self):
-#     return self.medicine_name  
+#     return self.medicine_name
   def has_perm(self, perm, obj=None):
       "Does the user have a specific permission?"
       # Simplest possible answer: Yes, always
@@ -125,19 +90,3 @@ class Pharmacy_medicine(models.Model):
       "Is the user a member of staff?"
       # Simplest possible answer: All admins are staff
       return self.is_admin
-
-#Offers Class
-class Offers(models.Model):
-   offer_name=models.CharField(max_length=255)
-   offer_image=models.ImageField(upload_to='img',null=False,verbose_name='Offer Image')
-   offer_description=models.TextField()
-   offer_previous_price=models.DecimalField(max_digits=6, decimal_places=2)
-   offer_new_price=models.DecimalField(max_digits=6,decimal_places=2)
-
-   def __str__(self):
-    return self.offer_name
-
-class Pharmacy_offers(models.Model):
-   pharmacy_id=models.ForeignKey(Pharmacy,on_delete=models.SET_NULL,null=True )
-   offer_id=models.ForeignKey(Offers,on_delete=models.SET_NULL,null=True )
- 
