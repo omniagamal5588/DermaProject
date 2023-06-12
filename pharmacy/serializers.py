9220fc30608453454c91e5a0198bc804fb74647d
eman-m0hamed <91141798+eman-m0hamed@users.noreply.github.com>
2023-06-12 18:37:05 +0300

    Update serializers.py

--- pharmacy/serializers.py
+++ pharmacy/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from pharmacy.models import Pharmacy,Medicine,Offers,Pharmacy_medicine,Subscription
+from pharmacy.models import Pharmacy,Medicine,Pharmacy_medicine,Subscription
 # from . import utils
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
@@ -9,7 +9,7 @@ from django.utils.encoding import smart_str, force_bytes, DjangoUnicodeDecodeErr
 
 #PharmacyRegisterSerializer
 class PharmacyRegistrationSerializer(serializers.ModelSerializer):
- 
+
   class Meta:
     model =Pharmacy
     fields=['name','email', 'phone_number', 'location', 'password','description','pharmacy_image']
@@ -21,7 +21,7 @@ class PharmacyRegistrationSerializer(serializers.ModelSerializer):
   def validate(self, attrs):
     password = attrs.get('password')
     #password2 = attrs.get('password2')
-    
+
 
     email = attrs.get('email', None)
     if Pharmacy.objects.filter(email=email).exists():
@@ -36,17 +36,12 @@ class PharmacyLoginSeraializer(serializers.ModelSerializer):
     model=Pharmacy
     fields=['email','password']
 
-# class PharmacyProfileSerializer(serializers.ModelSerializer):
-#   class Meta:
-#     model = Pharmacy
-#     fields = ['name','email', 'phone_number', 'location','description','pharmacy_image']
-
 
 #Pharmacy Profile
 class PharmacyProfileSerializer(serializers.ModelSerializer):
   class Meta:
     model = Pharmacy
-    fields = ['id', 'email', 'name','location','phone_number','pharmacy_image','description']    
+    fields = ['id', 'email', 'name','location','phone_number','pharmacy_image','description']
 
 
 class ResetPasswordSerializerPharma(serializers.Serializer):
@@ -60,33 +55,23 @@ class ResetPasswordSerializerPharma(serializers.Serializer):
 class MedicineSerializer(serializers.ModelSerializer):
   class Meta:
     model=Medicine
-    fields=['id','serial_number','medicine_image','medicine_name','medicine_description','medicine_price']
-  
+    fields=['id','serial_number','medicine_name','medicine_description','medicine_price', 'medicine_image']
+
 #Phamacy_medicine
 class PharmacyMedicineSerializer(serializers.ModelSerializer):
-  medicine_id = MedicineSerializer()
-  pharmacy_id = PharmacyProfileSerializer()
+  pharmacy_id = PharmacyProfileSerializer(read_only=True, allow_null=True)
+  medicine_id = MedicineSerializer(read_only=True, allow_null=True)
   class Meta:
     model=Pharmacy_medicine
-    fields=['pharmacy_id','medicine_id','offer']
+    # fields= "__all__"
+    fields = ['id', 'pharmacy_id', 'medicine_id', 'offer']
 
-#OffersSerializer
-class OfferSerializer(serializers.ModelSerializer):
-  offer_previous_price=serializers.DecimalField(required=True,write_only=True,max_digits=6,decimal_places=2)
-  offer_new_price=serializers.DecimalField(required=True,write_only=True,max_digits=6,decimal_places=2)
-
-  class Meta:
-    model=Offers
-    fields=['id','offer_name','offer_image','offer_description','offer_previous_price','offer_new_price']
-  #check if previous price is not equal new price 
-  def validate(self, data):
-    if data['offer_previous_price'] == data['offer_new_price']:
-      raise serializers.ValidationError("The two prices must be different")
-    return data  
 
 #SubscriptionPlanSerializer
 class SubscriptionPlanSerializer(serializers.ModelSerializer):
   class Meta:
     model=Subscription
     fields=['id','price','subscription_type','duration']
- 
+
+
+
