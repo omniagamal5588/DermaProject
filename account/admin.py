5ceb4b47665c23950cd1a998057a2472f214e2ac
Omnia Gamal <96677695+omniagamal5588@users.noreply.github.com>
2023-06-18 18:45:25 +0300

    Update admin.py

--- account/admin.py
+++ account/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from account.models import User
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('id', 'email', 'first_name', 'last_name', 'is_active','is_admin','created_at','phone_number')
+    list_display_links = ('id','email')
+    list_filter = ('is_active', 'created_at','is_admin')
+    search_fields = ( 'email', 'first_name', 'last_name')
+    ordering = ('-created_at',)
+    ###############################
+   
+admin.site.register(User, UserAdmin)
 
-admin.site.register(User)
+# admin.site.register(User)
+
