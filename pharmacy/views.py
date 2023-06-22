4b04a5f5ba522a69a1efb869bd15c527ab780f75
eman-m0hamed <91141798+eman-m0hamed@users.noreply.github.com>
2023-06-22 03:07:36 +0300

    Update views.py

--- pharmacy/views.py
+++ pharmacy/views.py
@@ -85,29 +85,6 @@ class PharmacyRegistrationView(APIView):
       if ExitPharmacy:
         return Response({"success":False,"message":"Pharmacy with this email already exist"},status=status.HTTP_201_CREATED)
         
-    if 'passowrd' not in request.data:
-      return Response({"success":False,"message":"password is required"},status=status.HTTP_201_CREATED)
-      
-    if 'phone_number' not in request.data:
-      return Response({"success":False,"message":"phone_number is required"},status=status.HTTP_201_CREATED)
-      
-    if 'location' not in request.data:
-      return Response({"success":False,"message":"location is required"},status=status.HTTP_201_CREATED)
-      
-    if 'pharmacy_image' not in request.data:
-      return Response({"success":False,"message":"pharmacy_image is required"},status=status.HTTP_201_CREATED)
-      
-    if 'description' not in request.data:
-      return Response({"success":False,"message":"description is required"},status=status.HTTP_201_CREATED)
-
-    # image_file = request.FILES.get('pharmacy_image')
-    if 'pharmacy_image' not in  request.FILES:
-        return Response({"success":False, 'message': 'Image file missed'}, status=status.HTTP_201_CREATED)
-    image_file = request.FILES.get('pharmacy_image')
-     
-    if not image_file.content_type.startswith('image'):
-        return Response({"success":False, 'message': 'File is not an image'}, status=status.HTTP_201_CREATED)
-      
     if serializer.is_valid(raise_exception=False):
         user = serializer.save()
         return Response({"success":True, 'message':'Registration Successfully'}, status=status.HTTP_201_CREATED)
